8f019219516608f3033251d5b6002fdaaaf527d1
Gabe Harvey <harvey7@gmail.com>
2024-08-22 10:21:21 -0500

    Google fonts and additional styling features added to MainContent.jsx

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,23 +3,23 @@ import { FaEnvelope, FaPhone, FaLinkedin } from 'react-icons/fa';
 
 const MainContent = () => {
   return (
-    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow="lg" p={6} bg="white">
+    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow="lg" p={6} bg="gray.900">
       <Flex direction="column" align="center">
-        <Image borderRadius="full" boxSize="150px" src="https://via.placeholder.com/150" alt="Profile Image" mb={4}/>
-        <Text fontSize="2xl" fontWeight="bold">Gabe Harvey</Text>
-        <Text fontSize="lg" color="gray.500" mb={4}>Full Stack Web Developer</Text>
-        <VStack align="start" spacing={2}>
+        <Image borderRadius="md" boxSize="150px" src="../src/assets/gabe-photo.jpg" alt="Profile Image" mb={4}/>
+        <Text fontSize="2xl" fontWeight="bold" color="beige" fontFamily="'Bungee Tint', cursive">Gabe Harvey</Text>
+        <Text fontSize="lg" color="beige" mb={4} fontFamily="'Marmelad', cursive">Full Stack Web Developer</Text>
+        <VStack align="start" spacing={2} fontFamily="'Marmelad', cursive">
           <HStack>
-            <Icon as={FaEnvelope} />
-            <Link href="mailto:gabeharvey11@gmail.com">gabeharvey11@gmail.com</Link>
+            <Icon as={FaEnvelope} color='white'/>
+            <Link href="mailto:gabeharvey11@gmail.com" color='beige'>gabeharvey11@gmail.com</Link>
           </HStack>
           <HStack>
-            <Icon as={FaPhone} />
-            <Text>(210) 219-3117</Text>
+            <Icon as={FaPhone} color='green.300'/>
+            <Text color='beige'>(210) 219-3117</Text>
           </HStack>
           <HStack>
-            <Icon as={FaLinkedin} />
-            <Link href="https://www.linkedin.com/in/gabe-harvey-37ab90308/" isExternal>linkedin.com/in/gabeharvey</Link>
+            <Icon as={FaLinkedin} color='royalblue' />
+            <Link href="https://www.linkedin.com/in/gabe-harvey-37ab90308/" isExternal color='beige'>linkedin.com/in/gabeharvey</Link>
           </HStack>
         </VStack>
       </Flex>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import MainContent from './components/MainContent.jsx'
+import { ChakraProvider } from '@chakra-ui/react'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ChakraProvider>
     <MainContent />
-  </StrictMode>,
+  </ChakraProvider>,
 )
